e77e330cc11c07ca3021f95126aebd54a7f79f1b
Guido do Rego <guidodorego@gmail.com>
2022-05-13 01:54:56 -0300

    Trabajando en funcion 2, agrego funcion 'solicitarNumLiquidacion' y empiezo a trabajar en la neuva: 'DepuradordeDuplicados'

--- 1.1copy.py
+++ 1.1copy.py
@@ -81,9 +81,9 @@ def Infodatabase():                                 #retorna una lista con lista
         input("Presione ENTER para continuar\n")
 
     finally:
-
         if onoffdatabase_close == 1:
             infodatabase.close()
+
         else:
             pass
 
@@ -235,6 +235,26 @@ def CopiarLiquidaciones():                                #Retorna una lista con
         archivoL.close()
 
 def Chequeoduplicados(listachica, listagrande): #Chequea si hay duplicados, de haberlos retorna cuales son sus num de indice en lista chica
+
+    """
+     _summary_
+    
+    por cada lindea de 'lista chica' chequea que no haya coinsidencias con 'lista grande'
+    
+    
+    Args:
+    listachica([[STR],[STR]]):Listas con strings
+    
+    listagrande([STR]): Lista de strings o un string solo dentro de una lista
+    
+    Returns:
+        indicesderepetidos([INT]): Un entero que indica el indice(en 'listachica') del str repetido
+        resultado1(False):Resultado por defecto que devuelve al no detectar duplicados
+    
+    """
+    
+    
+    
     resultado1= False
     numerodeserierepe=[]
     indicesderepetidos=[]
@@ -455,6 +475,111 @@ def SolicitarNuevoA ():
             print (f"Error tipo:{type(e).__name__} \n", e)
 
 
+def SolicitarNumLiquidacion():
+    
+    """
+    _summary_
+    Solicita un numero de liquidacion y controla que no sea 0, menor que 0 o que tenga letras
+
+    Returns:
+        _type_STR: _description_El numero de liquidacion solicitado(Ej:5871)
+    """
+    
+    while 1 == 1:
+        try:
+            
+            numerodeliquidacionsolicitado=int(input("Digite el numero de liquidacion:"))
+
+        
+            if numerodeliquidacionsolicitado <= 0:
+                print("El numero de liquidacion no puede ser menor o igual a 0")
+            
+            else:
+                numerodeliquidacionsolicitado=str(numerodeliquidacionsolicitado)
+                return numerodeliquidacionsolicitado
+
+        except ValueError as e:
+            print(f'\tError!!\nEl numero de liquidacion no admite letras')
+
+        except Exception:
+            print(f"\tError!!\nError de tipo:{type(e).__name__}\n{e}\n--------------------------------------")
+            input("Presione ENTER para continuar\n")
+
+def DepuradordeDuplicado(duplicados1, duplicados2, datoaescribir, datosexistentes):
+    """
+    DepuradordeDuplicado _summary_
+    
+    Chequea el informe de si hay duplicados y de haberlos da la opcion de:
+    Cancelar la operacion
+    Omitir nuevos duplicados
+    Eliminar los registros ya existentes para evitar duplicados1
+
+    duplicados1/duplicados2:
+    Resultado de funcion 'Chequeoduplicados1(a, b)', informe sobre si hay duplicados
+    
+    Args:
+        duplicados1 ([INT]/False): False/ Numero de index de los duplicados de los datos todavia no ingresados a la DB 
+        duplicados2 ([INT]/False): False/ Numero de index de los duplicados en la DB 
+        datoaescribir ([STR,STR,STR,STR,STR]): Lista con el registro a escribir indicando [año, mes, dia, monto, num de liquidacion(ID)] 
+        datosexistentes ([[str,..][str,..]]): Lista con listas que contienen los registros originales
+    """
+
+    if duplicados1 == False and duplicados2 == False:
+        Escribirbasededatos(datoaescribir)
+    
+    elif duplicados1 == False and duplicados2 != False or duplicados2 == False and duplicados1 != False:
+        print ("Error al intentar chequear la existencia de duplicados")
+        print ("la carga de datos se cancelara para intentar conservar la integridad de la base de datos")
+        input ("Presione ENTER para continuar:")
+    
+    else:
+        traba1=1
+        print("Se encontraron numeros de liquidacion duplicados con respecto a la base de datos.")
+        print("1-Omitir los NUEVOS importes duplicados\n2-Eliminar los importes duplicados EXISTENTES\n0-Para CANCELAR")
+        print("H-Texto un poco mas extenso descriptivo de opciones")
+        respuesta=str(input("1/2/0:"))
+        print("\n\n")
+        
+        while traba1 == 1:
+            
+            if respuesta == "1":
+                for i  in duplicados1:
+                    datoaescribir.pop(i)
+                Escribirbasededatos(datoaescribir)
+                traba1=0
+            
+            elif respuesta == "2":
+                for i  in duplicados2:
+                    datosexistentes.pop(i)
+                datosexistentes+=datoaescribir    
+                
+                Escribirbasededatos(datoaescribir)
+                traba1=0
+            
+            elif respuesta == "0":                    
+                onoffdeconfirmacion=0
+                traba1=0
+            
+            elif respuesta == "H" or respuesta == "h":
+                print("1-Elimina el/los registros nuevos que se detectar")
+                
+            else:
+                print(f'\nOpcion"{respuesta}" no encontrada, cancelando carga.\n')
+
+    if onoffdeconfirmacion == 1:
+        print(f'Se a pegado la informacion en la base de datos correctamente')
+        input("Presione ENTER para continuar\n")
+        limpiarpantalla()
+        
+    else:
+        print ("Se a Cancelado la carga de liquidacion en la base de datos.")
+        input("Presione ENTER para continuar\n")
+        limpiarpantalla()
+
+
+
+
+
 
 
 print("//////////////////////////////////")
@@ -506,36 +631,46 @@ while onoff==1:
         limpiarpantalla()
         print("2-Agregar un monto a la ficha de activos\n\n")
 
-
-
-        infodatabase=Infodatabase()
-        numerosdeseriedeDB=Solonumserie(infodatabase)
-
-        print (numerosdeseriedeDB)
-
-
         fechaenlista=fecha_actualvsingresada()
         
         añosolicitado=fechaenlista[0]
         messolicitado=fechaenlista[1]
         diasolicitado=fechaenlista[2]
 
-        montosolicitado=SolicitarNuevoA ()
-
-        numerodeliquidacionsolicitado=str(input("Digite el numero de liquidacion:")) #Es una especie de numero de serie para no repetir datos
+        montosolicitado=SolicitarNuevoA()
+        
+        numerodeliquidacionsolicitado=SolicitarNumLiquidacion()
+        
+        tablap=[numerodeliquidacionsolicitado]
 
         infodatabase=Infodatabase()
-        v=Chequeoduplicados2(numerodeliquidacionsolicitado, infodatabase)
+        solonumliquidacionDB=Solonumserie(infodatabase)
+        
+        
+        print (solonumliquidacionDB)
+        print (tablap)
+        
+        duplicados=Chequeoduplicados(solonumliquidacionDB, tablap)
+
+        print("//////////////////////////////")
+        
+        print (duplicados)
+        input("AA")
+
+        if duplicados == False:
+            pass
+            datos_a_pegar=[[añosolicitado, messolicitado, diasolicitado, montosolicitado, numerodeliquidacionsolicitado]]
+            Escribirbasededatos(datos_a_pegar)  
+            print(f'Se a pegado "{datos_a_pegar}" correctamente')
+            input("Presione ENTER para continuar\n")
+            limpiarpantalla()
+        
+        else:
+            
+            pass
 
 
 
-        datos_a_pegar=[[añosolicitado, messolicitado, diasolicitado, montosolicitado, numerodeliquidacionsolicitado]]
-
-        Escribirbasededatos(datos_a_pegar)
-
-        print(f'Se a pegado "{datos_a_pegar}" correctamente')
-        input("Presione ENTER para continuar\n")
-        limpiarpantalla()
 
     elif cursor=="3" or cursor=="3-": #Agregar un monto a la ficha de pasivos
         limpiarpantalla()
@@ -581,7 +716,7 @@ while onoff==1:
             elif respuesta == "0":                                   #Misma posibilidad de recortar                     
                 onoffdeconfirmacion=0
             else:
-                print(f'Opcion"{respuesta}" no encontrada.')
+                print(f'Opcion"{respuesta}" no encontrada, cancelando carga.')
                 onoffdeconfirmacion=0
 
         if onoffdeconfirmacion == 1:
